0bd5ead5a468b01b3c7413e5bc4a302c7463b219
Kateryna Romanchenko <katerynar@compatibl.com>
2023-05-24 11:32:18 +0300

    Autorisation with database was added.

---
+++ MoneyGuru/MoneyGuru/Data/User.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class User
+{
+    [PrimaryKey, AutoIncrement]
+    public int ID { get; set; }
+
+    [Unique]
+    public string Email { get; set; }
+    public string Password { get; set; }  //Зробити хешування
+    public string Token { get; set; }
+}
---
+++ MoneyGuru/MoneyGuru/Views/SignInPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Xamarin.Forms;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System;
+using System.Linq;
+
+namespace MoneyGuru
+{
+    public partial class SignInPage : ContentPage
+    {
+        public SignInPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnRegisterClicked(object sender, EventArgs e)
+        {
+            var existingUser = App.Database.Table<User>().FirstOrDefault(x => x.Email == EmailEntry.Text);
+            if (existingUser == null)
+            {
+                // Register the user here
+                var newUser = new User
+                {
+                    Email = EmailEntry.Text,
+                    Password = PasswordEntry.Text,
+                    Token = Guid.NewGuid().ToString(), //Generating a new GUID as a token
+
+                };
+                App.Database.Insert(newUser);
+                await DisplayAlert("Success", "User created successfully", "OK");
+                await Navigation.PopAsync(); //go back to the login page
+            }
+            else
+            {
+                await DisplayAlert("Error", "User with this email already exists", "OK");
+            }
+        }
+    }
+}
